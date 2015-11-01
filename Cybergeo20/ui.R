6b73c3bb7627194d0a7c705ba1129ee0394c62a6
Clémentine Cottineau <clementine.cottineau@iscpif.fr>
2015-11-01 13:30:23 +0000

    add panels to app

--- Cybergeo20/ui.R
+++ Cybergeo20/ui.R
@@ -9,28 +9,52 @@ shinyUI(fluidPage(theme = "darkBlue.css",
     "))
   ),
   headerPanel(
-    h1("Cybergeo | 1996-2016", 
+    h1("Cybergeo  |  1996-2016", 
        style = "font-family: 'Orbitron', sans-serif;
         font-weight: 500; line-height: 1.1; 
         color: #ffffff;")),
-  
-  
- # titlePanel(""),
-  titlePanel(h4("  20 years of Cybergeo, European Journal of Geography")),
+  titlePanel(h4("20 years of Cybergeo, European Journal of Geography")),
   
   navlistPanel(
     #"Looking back",
     tabPanel("The Project",
              fluidRow(
-               column(3, img(src = "favicon.png",class="img-responsive")),
+               
                column(9, h2("20 Years of Cybergeo"),
                tags$p(class="text-justify",
-              "Cybergeo turns 20 this years. It's time to look back ... 
-              [blablabla]", br(), 
-              "This app aims at reviewing the evolution of geography as revealed by 20 years of publication in Cybergeo.")
-               ))
-             )
+              "Cybergeo turns 20: It's time to look back ... ", br(), "[blablabla]", br())) ,
+              column(3, img(src = "favicon.png",class="img-responsive")),
+              column(12,h4("The journal philosophy"), 
+              "[blablabla]", br(), 
+               h4("An app to look back"), "This app aims at reviewing the evolution of geography as revealed by 20 years of publication in Cybergeo.")
+               )
+             ),
+    "-----",
+    tabPanel("Global Stats",
+             fluidRow(h2("Global Stats"),
+                      h4("Number of papers / Authors / Editos / Reviews",br()), 
+                         h4("Number of readers / countries / citations",br()), 
+                            h4("Number of staff")
+             )),
+    
+    tabPanel("Citation network",
+             fluidRow(h2("Exploring the citation network"),
+                      h4("...",br()))
     )
-                             
+   ,
+       
+  "-----",
+  tabPanel("About",
+           fluidRow(
+             
+             column(9, h2("About Cybergeo"),
+                    a("http://cybergeo.revues.org/",href="http://cybergeo.revues.org/"), br(),
+                    "[blablabla]", br()),
+             column(3, img(src = "favicon.png",class="img-responsive")),
+             column(12,h4("The Cybergeo20 team"), 
+                    "[blablabla]", br())
+             )
+  )
   
+  )
 ))
