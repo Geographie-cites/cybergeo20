33c9941f3d173058405a83cd7658df2d4c5f0c1d
JusteRaimbault <raimbaultjwin@gmail.com>
2016-01-04 17:55:04 +0100

    kw_extraction

--- CitationNetwork/Models/Semantic/kwExtraction.py
+++ CitationNetwork/Models/Semantic/kwExtraction.py
@@ -1,13 +1,79 @@
+import nltk,MySQLdb,time,locale
+
+
+def run :
+    run_kw_extraction()
 
 
 
-def test :
+
+# read a conf file under the format key:value
+# , returns a dictionary
+def read_conf(file):
+    conf = open(file,'r')
+    res=dict()
+    currentLine = conf.readline().replace('\n','')
+    while currentLine != '' :
+        t=str.split(currentLine,':')
+        if len(t) != 2 : raise Exception('error in conf file')
+        res[t[0]]=t[1]
+        currentLine = conf.readline().replace('\n','')
+        #print(t[0]+' : '+t[1])
+    return(res)
 
 
 
 
+def run_kw_extraction() :
+    start = time.time()
+    while True :
+        # get data from mysql
+        conf=read_conf('conf/mysql.conf')
+        user = conf['user']
+        password = conf['password']
+        db = MySQLdb.connect("localhost",user,password,"cybergeo")
 
 
+
+
+def potential_multi_term(tagged) :
+    res = True
+    for tag in tagged :
+        res = res and (tag[1]=='NN' or tag[1]=='NNP' or tag[1] == 'VBG' or tag[1] =='NNS'or tag[1] =='JJ' or tag[1] =='JJR')
+    return res
+
+
+def extract_keywords(raw_text,id):
+
+    print("Extracting keywords for "+id)
+
+    stemmer = nltk.PorterStemmer()
+
+    # Construct text
+
+    # Tokens
+    tokens = nltk.word_tokenize(raw_text)
+    # filter undesirable words and format
+    words = [w.replace('\'','') for w in tokens if len(w)>=3]
+    text = nltk.Text(words)
+
+    tagged_text = nltk.pos_tag(text)
+    #nouns = [tg[0] for tg in tagged_text if tg[1]=='NN' or tg[1]=='NNP' ]
+    #print(nouns)
+
+    # multi-term
+    multiterms = []
+    for i in range(len(tagged_text)) :
+        # max length 4 for multi-terms
+        for l in range(1,5) :
+            if i+l < len(tagged_text) :
+                tags = [tagged_text[k] for k in range(i,i+l)]
+                if potential_multi_term(tags) :
+                    multistem = [str.lower(stemmer.stem(tagged_text[k][0]).encode('ascii','ignore')) for k in range(i,i+l)]
+                    multistem.sort(key=str.lower)
+                    multiterms.append(multistem)
+
+    return multiterms
 
 
 
@@ -18,7 +84,7 @@ def main():
 
     start = time.time()
 
-    test()
+    run()
 
     print('Ellapsed Time : '+str(time.time() - start))
 
