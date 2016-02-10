9121e859ece84b5d4ff1390e97f8392caae61619
Juste Raimbault <juste.raimbault@polytechnique.edu>
2016-02-10 22:03:42 +0100

    recompute_cyb_kws

--- HyperNetwork/Models/Semantic/cybergeo.py
+++ HyperNetwork/Models/Semantic/cybergeo.py
@@ -1,8 +1,8 @@
 #import utils,kwExtraction
 # kw extraction for cybergeo corpus alone
 import utils,kwFunctions
-from lxml import html,etree
-import kwExtraction
+#from lxml import html,etree
+#import kwExtraction
 
 def extract_cybergeo_keywords():
     data=utils.get_data('SELECT refdesc.id,refdesc.abstract FROM refdesc INNER JOIN cybergeo ON cybergeo.id=refdesc.id;','mysql') # WHERE abstract IS NOT NULL AND abstract!=\'\';','mysql')
@@ -42,5 +42,9 @@ def extract_relevant_cybergeo (kwLimit,database) :
     [relevantkw,relevant_dico] = kwFunctions.extract_relevant_keywords(corpus,kwLimit,occurence_dicos)
     utils.export_dico_csv(relevant_dico,'res/cybergeo/relevantDico_kwLimit'+str(kwLimit),False)
     utils.export_dico_num_csv(relevantkw,'res/cybergeo/kw_'+str(kwLimit),False)
+
+
+for kwLimit in [100,200,500,1000,1500,2000]:
+    extract_relevant_cybergeo (kwLimit,'../../Data/dumps/20160210_cybergeo.sqlite3')
 
 
