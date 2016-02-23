7a4c7c78e6611ad33415d05c9f14d00191f73d85
JusteRaimbault <raimbaultjwin@gmail.com>
2016-02-23 19:01:07 +0100

    ignore_author_data_in_case_repo_goes_public_to_avoid_CYBERGEO-LEAKS_scandal

--- HyperNetwork/Models/Semantic/bootstrap.py
+++ HyperNetwork/Models/Semantic/bootstrap.py
@@ -146,51 +146,52 @@ def bootstrap_subcorpuses(corpus,occurence_dicos,kwLimit,subCorpusSize,bootstrap
     return(res)
 
 
-def bootrun(extraction,corpus,kwLimit,occurence_dicos):
-    subcorpus = [corpus[i] for i in extraction]
-    return(kwFunctions.extract_relevant_keywords(subcorpus,kwLimit,occurence_dicos))
+#def bootrun(extraction,corpus,kwLimit,occurence_dicos):
+#    subcorpus = [corpus[i] for i in extraction]
+#    return(kwFunctions.extract_relevant_keywords(subcorpus,kwLimit,occurence_dicos))
 
 
 
 ##
 # with parallel processing
-def bootstrap_subcorpuses_parallel(corpus,kwLimit,subCorpusSize,bootstrapSize):
-    N = len(corpus)
-
-    print('Parallel bootstrapping on corpus of size '+str(N))
-
-    occurence_dicos = utils.import_kw_dico('../../Data/dumps/20160125_cybergeo.sqlite3')
-
-    # generate bSize extractions
-    #   -> random subset of 1:N of size subCorpusSize
-    # TODO extractions = [[numpy.random.random_integers(0,(N-1),subCorpusSize),corpus,kwLimit,occurence_dicos] for b in range(bootstrapSize)]
-
-    mean_termhoods = dict() # mean termhoods progressively updated
-    ref_kw_dico = dict() # ref -> kw dico : cumulated on repetitions. if a kw is relevant a few time, counted as 0 in mean.
-
-    #for eind in range(len(extractions)) :
-    #    print("bootstrap : run "+str(eind))
-    #    extraction = extractions[eind]
-    #    subcorpus = [corpus[i] for i in extraction]
-    #    [keywords,ref_kw_local_dico] = kwFunctions.extract_relevant_keywords(subcorpus,kwLimit,occurence_dicos)
-
-    p = Pool(15)
-    res = p.map(bootrun,extractions)
-
-    for singleres in res :
-	keywords = singleres[0]
-	ref_kw_local_dico = singleres[1]
-
-        # add termhoods
-        for kw in keywords.keys() :
-            if kw not in mean_termhoods : mean_termhoods[kw] = 0
-            mean_termhoods[kw] = mean_termhoods[kw] + keywords[kw]
-
-        # update p->kw dico
-        for ref in ref_kw_local_dico.keys() :
-            if ref not in ref_kw_dico : ref_kw_dico[ref] = set()
-            for kw in ref_kw_local_dico[ref] :
-                       ref_kw_dico[ref].add(kw)
-
-    # sort on termhoods (no need to normalize) adn returns
-    return(kwFunctions.extract_from_termhood(mean_termhoods,ref_kw_dico,kwLimit))
+#def bootstrap_subcorpuses_parallel(corpus,kwLimit,subCorpusSize,bootstrapSize):
+#    N = len(corpus)
+#
+#    print('Parallel bootstrapping on corpus of size '+str(N))
+#
+#    occurence_dicos = utils.import_kw_dico('../../Data/dumps/20160125_cybergeo.sqlite3')
+#
+#    # generate bSize extractions
+#    #   -> random subset of 1:N of size subCorpusSize
+#    # extractions = [[numpy.random.random_integers(0,(N-1),subCorpusSize),corpus,kwLimit,occurence_dicos] for b in range(bootstrapSize)]
+#
+#    mean_termhoods = dict() # mean termhoods progressively updated
+#    ref_kw_dico = dict() # ref -> kw dico : cumulated on repetitions. if a kw is relevant a few time, counted as 0 in mean.
+#
+#    #for eind in range(len(extractions)) :
+#    #    print("bootstrap : run "+str(eind))
+#    #    extraction = extractions[eind]
+#    #    subcorpus = [corpus[i] for i in extraction]
+#    #    [keywords,ref_kw_local_dico] = kwFunctions.extract_relevant_keywords(subcorpus,kwLimit,occurence_dicos)
+#
+#    p = Pool(15)
+#    res = p.map(bootrun,extractions)
+#
+#    for singleres in res :
+#	keywords = singleres[0]
+#	ref_kw_local_dico = singleres[1]
+#
+#        # add termhoods
+#        for kw in keywords.keys() :
+#            if kw not in mean_termhoods : mean_termhoods[kw] = 0
+#            mean_termhoods[kw] = mean_termhoods[kw] + keywords[kw]
+#
+#        # update p->kw dico
+#        for ref in ref_kw_local_dico.keys() :
+#            if ref not in ref_kw_dico : ref_kw_dico[ref] = set()
+#            for kw in ref_kw_local_dico[ref] :
+#                       ref_kw_dico[ref].add(kw)
+#
+#    # sort on termhoods (no need to normalize) adn returns
+#    return(kwFunctions.extract_from_termhood(mean_termhoods,ref_kw_dico,kwLimit))
+#
