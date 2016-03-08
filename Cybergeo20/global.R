bd8a54ea332093c0a5ebdad54a921a149b690693
hcommenges <hcommenges@parisgeo.cnrs.fr>
2016-03-08 22:32:11 +0100

    add keywords network

---
+++ Cybergeo20/global.R
@@ -0,0 +1,139 @@
+##############################
+# Shiny App: Cybergeo20
+# Packages and functions
+##############################
+
+
+
+# load packages ----
+
+library(shiny)
+library(rgdal)
+library(plyr)
+library(rgdal) 
+library(mapproj) 
+library(maptools) 
+library(RColorBrewer)
+library(RCurl)
+library(ggplot2)
+library(reshape2)
+library(grid)
+library(igraph)
+library(dplyr)
+
+
+# plot communities ----
+
+VisuComm <- function(g, comm, vertcol, vertsize, vfacsize, edgesize, efacsize, textsize){
+  # circle layout with sampled coordinates
+  oriCoords <- layout_in_circle(g)
+  corrCoords <- oriCoords[sample(seq(1, nrow(oriCoords), 1), size = nrow(oriCoords), replace = FALSE), ]
+  
+  plot(g,
+       main = paste("Communauté \"", comm, "\" - ", sep = ""),
+       edge.color = "grey60",
+       edge.width = efacsize * edgesize,
+       edge.curved = F,
+       edge.arrow.mode = "-",
+       edge.arrow.size = 0.01,
+       vertex.color = vertcol,
+       vertex.frame.color = "white",
+       vertex.label = V(g)$name,
+       vertex.label.color = "black",
+       vertex.label.family = "sans-serif",
+       vertex.label.cex = textsize / 10,
+       vertex.size = vfacsize * vertsize,
+       layout = corrCoords
+  )
+}
+
+
+
+# plot semantic field ----
+
+VisuSem <- function(g, kw, textsizemin, textsizemax){
+  
+  # make theme empty
+  theme_empty <- theme_bw() +
+    theme(axis.line = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          panel.background = element_rect(fill = "#e6e6e6"),
+          axis.title = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          legend.position = "bottom")
+  
+  # get title
+  plotTitle <- paste(kw, ": semantic area", sep="")
+  
+  # graph layout
+  tabPoints <- get.data.frame(x = g, what = "vertices")
+  tabLinks <- get.data.frame(x = g, what = "edges")
+  tabLinks$NODES <- ifelse(tabLinks$from == kw, tabLinks$to, tabLinks$from)
+  tabPoints <- tabPoints %>% left_join(x = ., y = tabLinks, by = c("name" = "NODES"))
+  
+  # compute distance from ego
+  tabPoints$DIST <- 1 / tabPoints$relresid
+  thresRadis <- seq(0, 0.1 + max(tabPoints$DIST, na.rm = TRUE), 0.1)
+  tabPoints$X <- cut(tabPoints$DIST, breaks = thresRadis, labels = thresRadis[-1], include.lowest = TRUE, right = FALSE)
+  tabPoints <- tabPoints %>% group_by(X) %>% mutate(NPTS = n())
+  
+  # get x values
+  tabPoints <- tabPoints %>% do(GetXvalues(df = .))
+  tabPoints[tabPoints$name == kw, c("XVAL", "DIST")] <- c(0, 0)
+  
+  # prepare plot
+  tabPoints$RESIDCLASS <- ifelse(tabPoints$DIST == 0, "EGO", 
+                                 ifelse(tabPoints$DIST < 1, "POSITIVE", "NEGATIVE"))
+  tabPoints$IDEGO <- ifelse(tabPoints$name == kw, 2, 1)
+  tabCircle <- data.frame(XVAL = c(0, 360), DIST = 1)
+  
+  # draw plot
+  circVis <- ggplot() + 
+    geom_line(data = tabCircle, aes(x = XVAL, y = DIST)) + 
+    geom_text(data = tabPoints, aes(x = XVAL, y = DIST, label = name, colour = RESIDCLASS, fontface = IDEGO, size = nbauth)) +
+    scale_colour_manual("Type", values = c("grey28", "firebrick1", "deepskyblue")) +
+    scale_size_continuous("Number of articles", range = c(textsizemin, textsizemax)) +
+    coord_polar(theta = "x") +
+    theme_empty +
+    labs(title = plotTitle)
+  
+  return(circVis)
+}
+
+
+
+
+
+# Internal functions for VisuSem() ----
+
+
+# Sample x values for polar coordinates
+
+GetXvalues <- function(df){
+  initVal <- sample(x = 0:360, size = 1, replace = FALSE)
+  tempRange <- seq(initVal, initVal + 360, 360/unique(df$NPTS))
+  tempRange <- tempRange[-length(tempRange)]
+  df$XVAL <- ifelse(tempRange > 360, tempRange - 360, tempRange) 
+  return(df)
+}
+
+
+# create semantic field network
+
+SemanticField <- function(g, kw){
+  
+  # list of neighbors
+  neiNodes <- unlist(neighborhood(g, order = 1, nodes = V(g)[V(g)$name == kw], mode = "all"))
+  pairedNodes <- unlist(paste(which(V(g)$name == kw), neiNodes[-1], sep = ","))
+  collapseNodes <- paste(pairedNodes, collapse = ",")
+  vecNodes <- as.integer(unlist(strsplit(collapseNodes, split = ",")))
+  
+  # get edges and create graph
+  edgeIds <- get.edge.ids(g, vp = vecNodes)
+  gSem <- subgraph.edges(g, eids = edgeIds, delete.vertices = TRUE)
+  
+  return(gSem)
+}
--- Cybergeo20/ui.R
+++ Cybergeo20/ui.R
@@ -1,65 +1,156 @@
 library(shiny)
 
 shinyUI(fluidPage(theme = "darkBlue.css",
-  #tags$head(tags$link(rel="shortcut icon", href="favicon.png")),
-  
-  tags$head(
-    tags$style(HTML("
+                  #tags$head(tags$link(rel="shortcut icon", href="favicon.png")),
+                  
+                  tags$head(
+                    tags$style(HTML("
       @import url('//fonts.googleapis.com/css?family=Orbitron|Cabin:400,700');
     "))
-  ),
-  headerPanel(
-    h1("Cybergeo  |  1996-2016", 
-       style = "font-family: 'Orbitron', sans-serif;
+                  ),
+                  headerPanel(
+                    h1("Cybergeo  |  1996-2016", 
+                       style = "font-family: 'Orbitron', sans-serif;
         font-weight: 500; line-height: 1.1; 
         color: #ffffff;")),
-  titlePanel(h4("20 years of Cybergeo, European Journal of Geography")),
-  
-  navlistPanel(
-    #"Looking back",
-    tabPanel("The Project",
-             fluidRow(
-               
-               column(9, h2("20 Years of Cybergeo"),
-               tags$p(class="text-justify",
-              "Cybergeo turns 20: It's time to look back ... ", br(), "[blablabla]", br())) ,
-              column(3, img(src = "favicon.png",class="img-responsive")),
-              column(12,h4("The journal philosophy"), 
-              "[blablabla]", br(), 
-               h4("An app to look back"), "This app aims at reviewing the evolution of geography as revealed by 20 years of publication in Cybergeo.")
-               )
-             ),
-    "-----",
-    tabPanel("Global Stats",
-             fluidRow(h2("Global Stats"),
-                      column(12, sliderInput("dateRange", label = "Time Range",
-                                            min = 1996, max = 2015, value = c(1996,2015), step = 1)),
-                      column(12, dataTableOutput('statArticles')),
-                      div(dataTableOutput("tableName"), style = "font-size:120%")
-                      #h4("Number of papers / Authors / Editos / Reviews",br()), 
-                      
-                       #  h4("Number of readers / countries / citations",br()), 
-                        #    h4("Number of staff")
-             )),
-    
-    tabPanel("Citation network",
-             fluidRow(h2("Exploring the citation network"),
-                      h4("...",br()))
-    )
-   ,
-       
-  "-----",
-  tabPanel("About",
-           fluidRow(
-             
-             column(9, h2("About Cybergeo"),
-                    a("http://cybergeo.revues.org/",href="http://cybergeo.revues.org/"), br(),
-                    "[blablabla]", br()),
-             column(3, img(src = "favicon.png",class="img-responsive")),
-             column(12,h4("The Cybergeo20 team"), 
-                    "[blablabla]", br())
-             )
-  )
-  
-  )
-))
+                  titlePanel(h4("20 years of Cybergeo, European Journal of Geography")),
+                  
+                  navlistPanel(
+                    #"Looking back",
+                    tabPanel("The Project",
+                             fluidRow(
+                               
+                               column(9, h2("20 Years of Cybergeo"),
+                                      tags$p(class="text-justify",
+                                             "Cybergeo turns 20: It's time to look back ... ", br(), "[blablabla]", br())) ,
+                               column(3, img(src = "favicon.png",class="img-responsive")),
+                               column(12,h4("The journal philosophy"), 
+                                      "[blablabla]", br(), 
+                                      h4("An app to look back"), "This app aims at reviewing the evolution of geography as revealed by 20 years of publication in Cybergeo.")
+                             )
+                    ),
+                    "-----",
+                    tabPanel("Global Stats",
+                             fluidRow(h2("Global Stats"),
+                                      column(12, sliderInput("dateRange", label = "Time Range",
+                                                             min = 1996, max = 2015, value = c(1996,2015), step = 1)),
+                                      column(12, dataTableOutput('statArticles')),
+                                      div(dataTableOutput("tableName"), style = "font-size:120%")
+                                      #h4("Number of papers / Authors / Editos / Reviews",br()), 
+                                      
+                                      #  h4("Number of readers / countries / citations",br()), 
+                                      #    h4("Number of staff")
+                             )),
+                    
+                    tabPanel("Citation network",
+                             fluidRow(h2("Exploring the citation network"),
+                                      h4("...",br()))
+                    ),
+                    
+                    # HADRI ----
+                    
+                    tabPanel("Keyword network",
+                             
+                             tabsetPanel(
+                               
+                               tabPanel("Data summary",
+                                        fluidRow(
+                                          htmlOutput("textfull"),
+                                          tags$hr(),
+                                          dataTableOutput("contentsnodes"),
+                                          dataTableOutput("contentsedges")
+                                        )),
+                               
+                               tabPanel("Communities",
+                                        fluidRow(
+                                          column(8, selectInput(inputId = "commid", 
+                                                                label = "Choose a community", 
+                                                                choices = "",
+                                                                selected = "",
+                                                                multiple = FALSE))),
+                                        fluidRow(
+                                          column(12, 
+                                                 fluidRow(
+                                                   column(3, wellPanel(
+                                                     tags$strong("Graphic options"),
+                                                     radioButtons("vsizecom", "Nodes proportional to:",
+                                                                  list("Uniforme" = "uni",
+                                                                       "Number of articles" = "poi",
+                                                                       "Nodes degree" = "deg")),
+                                                     radioButtons("esizecom", "Edges proportional to:",
+                                                                  list("Observed weight" = "nbl",
+                                                                       "Residuals" = "rel")),
+                                                     sliderInput(inputId = "vfacsizecom", 
+                                                                 label = "Nodes size", 
+                                                                 min = 0, 
+                                                                 max = 1, 
+                                                                 value = 0.5, 
+                                                                 step = 0.05),
+                                                     sliderInput(inputId = "efacsizecom", 
+                                                                 label = "Edges size", 
+                                                                 min = 0, 
+                                                                 max = 2, 
+                                                                 value = 1, 
+                                                                 step = 0.1),
+                                                     sliderInput(inputId = "tsizecom",
+                                                                 label = "Font size", 
+                                                                 min = 1, 
+                                                                 max = 15, 
+                                                                 value = 10, 
+                                                                 step = 1),
+                                                     downloadButton("downcomm", "Download plot"))),
+                                                   
+                                                   column(9, plotOutput("plotcomm", width = "100%", height = "800px")))))
+                               ),
+                               
+                               
+                               tabPanel("Semantic area",
+                                        fluidRow(
+                                          column(8, selectInput(inputId = "kwid2", 
+                                                                label = "Choose a keyword", 
+                                                                choices = "", 
+                                                                selected = "", 
+                                                                multiple = FALSE))),
+                                        fluidRow(
+                                          column(12, 
+                                                 fluidRow(
+                                                   column(3, wellPanel(
+                                                     tags$strong("Graphic options"),
+                                                     sliderInput(inputId = "tsizesemmin", 
+                                                                 label = "Font size (min)", 
+                                                                 min = 1, 
+                                                                 max = 10, 
+                                                                 value = 4, 
+                                                                 step = 0.5),
+                                                     sliderInput(inputId = "tsizesemmax", 
+                                                                 label = "Font size (max)", 
+                                                                 min = 1, 
+                                                                 max = 10, 
+                                                                 value = 6, 
+                                                                 step = 0.5),
+                                                     downloadButton("downsem", "Download plot"))),
+                                                   
+                                                   column(9, plotOutput("plotsem", width = "100%", height = "800px")))))
+                               ),
+                               
+                               tabPanel("User guide",
+                                        withMathJax(), 
+                                        includeMarkdown("README.md"))
+                             )
+                    ),
+                    
+                    
+                    "-----",
+                    tabPanel("About",
+                             fluidRow(
+                               
+                               column(9, h2("About Cybergeo"),
+                                      a("http://cybergeo.revues.org/",href="http://cybergeo.revues.org/"), br(),
+                                      "[blablabla]", br()),
+                               column(3, img(src = "favicon.png",class="img-responsive")),
+                               column(12,h4("The Cybergeo20 team"), 
+                                      "[blablabla]", br())
+                             )
+                    )
+                    
+                  )))
