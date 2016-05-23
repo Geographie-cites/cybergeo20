7cf1f9deaa126997f25fd5e14e661bff81e4c075
Clémentine Cottineau <clementine.cottineau@iscpif.fr>
2016-05-23 20:45:39 +0100

    add user guide for geosemantic networks

--- Cybergeo20/ui.R
+++ Cybergeo20/ui.R
@@ -26,17 +26,16 @@ shinyUI(fluidPage(theme = "darkBlue.css",
                                       tags$p(class="text-justify",
                                              "Cybergeo turns 20: it’s time to look back for reflection and to anticipate future evolution!", br())) ,
                                column(3, img(src = "favicon.png",class="img-responsive")),
-                               column(12,h4("The journal philosophy"), 
-                                      tags$p(class="text-justify", "First entirely electronic journal for social sciences in the world, 
-                                      peer reviewed, European, open (free of charge for authors and readers), 
-                                      with a focus on geography and widely open to the diversity of research agendas and methodologies in all countries. 
+                               column(12,h4("The editorial policy"), 
+                                      tags$p(class="text-justify", "First entirely electronic journal for social sciences in the world, peer reviewed, European, open (free of charge for authors and readers), with a focus on geography and widely open to the diversity of research agendas and methodologies in all countries.
                                       Cybergeo is a success story with now more than one million papers downloaded every year."), br(), 
                                       h4("An app to look back"), 
                                       tags$p(class="text-justify", "This app builds on 20 years of publication in Cybergeo.
-                                      You can play with the data, draw geographical networks of authoring, 
-                                      studying and citing through countries, analyze semantic networks per key words and articles’ content, 
-                                      you can follow twenty years of epistemological trends in a variety of fields of scientific interest. Data are regularly updated.")
-                             ))
+                                     You can play with data, drawing geographical networks of authoring, 
+                                      studying and citing through countries, analyzing semantic networks per key words and articles’ content, 
+                                      you can review twenty years of epistemological and thematic trends in a variety of fields of scientific interest. 
+                                        The networks tell who studies what, where and how. Data are regularly updated."         
+                                             )))
                     ),
                     "-----",
                     tabPanel("Overview",
@@ -213,10 +212,11 @@ shinyUI(fluidPage(theme = "darkBlue.css",
                                       "Pierre-Olivier Chasset", a("(@chasset)",href="https://github.com/chasset"), br(),
                                       "Hadrien Commenges", a("(@hcommenges)",href="https://github.com/hcommenges"), br(),
                                       "Clémentine Cottineau", a("(@ClementineCttn)",href="https://github.com/ClementineCttn"), br(),
-                                      "Juste Raimbault", a("(@JusteRaimbault)",href="https://github.com/JusteRaimbault"), br(),
                                       "Antoine Fleury", br(),
                                       "Christine Kosmopoulos", br(),
-                                      "Denise Pumain")
+                                      "Denise Pumain", br(),
+                                      "Juste Raimbault", a("(@JusteRaimbault)",href="https://github.com/JusteRaimbault")
+                               )
                              )
                     )
                     
