bf381ee077d606fa864bc83d748c7708b857e443
JusteRaimbault <raimbaultjwin@gmail.com>
2016-05-26 15:54:27 +0200

    added_analytics_script

--- CybergeoNetworks/ui.R
+++ CybergeoNetworks/ui.R
@@ -6,7 +6,16 @@ shinyUI(fluidPage(theme = "darkBlue.css",
                   tags$head(
                     tags$style(HTML("
       @import url('//fonts.googleapis.com/css?family=Orbitron|Cabin:400,700');
-    "))
+    ")),
+            tags$script(HTML("
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40299595-6', 'auto');
+  ga('send', 'pageview');
+"))
                   ),
                   headerPanel(
                     "CybergeoNetworks",
