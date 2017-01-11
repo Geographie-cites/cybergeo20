c875b8f80cc1a6bfb6260f12f65555445a068b0b
JusteRaimbault <raimbaultjwin@gmail.com>
2017-01-11 16:09:17 +0800

    some_cleaning

--- HyperNetwork/Models/Semantic/main.py
+++ HyperNetwork/Models/Semantic/main.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 import time
 
-import bootstrap,utils,stats,cybergeo
+import relevant,utils,stats,cybergeo
 
 
 def run():
+    ## extract keywords
     #run_kw_extraction()
-    #bootstrap.test_bootstrap()
-    #utils.mysql2sqlite('../../Data/dumps/20160126_cybergeo.sqlite3')
-    #bootstrap.test_bootstrap()
+
+    ## stats
     #stats.export_ref_info()
-    #bootstrap.init_bootstrap('relevant_full_'+str(20000))
-    #bootstrap.run_bootstrap('test_kw1000_csize5000_b20',1000,5000,20,1)
-    bootstrap.relevant_full_corpus(50000)
-    #bootstrap.run_bootstrap('bootstrap/run_kw1000_csize5000_b20',1000,5000,20,10)
+
+    ## relevance estimation
+    relevant.relevant_full_corpus(50000)
+
+
     #cybergeo.extract_cybergeo_keywords()
     #cybergeo.extract_relevant_cybergeo(2000)
     #cybergeo.extract_relevant_cybergeo_fulltext(20)
